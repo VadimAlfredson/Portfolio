a58b9448d426c42d15440732dfd5781f78b454f4
VadimAlfredson <damonkvantel@gmail.com>
2023-07-30 23:03:04 +0500

    gh-pages

--- src/components/Content/Main/Main.tsx
+++ src/components/Content/Main/Main.tsx
@@ -1,10 +1,13 @@
 import React, {FC} from "react";
 import { useTranslation } from "react-i18next";
-import "../../../utils/i18next"
+import "../../../utils/i18next";
+import s from "./main.module.css"
 
 const Main: FC = () => {
     const { t } = useTranslation();
-    return <div>{t("main.name")}</ div>
+    return <div><h1 className={s.name}>{t("main.name")}</h1>
+    <p className={s.about}>{t("main.about")}</p>
+    </ div>
 }
 
 export default Main
