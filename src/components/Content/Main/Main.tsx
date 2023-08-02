1941bd2da3eb94de4a5d95b1246bbc445bfa1f11
VadimAlfredson <damonkvantel@gmail.com>
2023-08-02 19:38:25 +0500

    main

--- src/components/Content/Main/Main.tsx
+++ src/components/Content/Main/Main.tsx
@@ -5,9 +5,12 @@ import s from "./main.module.css"
 
 const Main: FC = () => {
     const { t } = useTranslation();
-    return <div><h1 className={s.name}>{t("main.name")}</h1>
+    return <div className={s.main}>
+        <div className={s.photo}>картинка</div>
+        <div><h1 className={s.name}>{t("main.name")}</h1>
     <p className={s.about}>{t("main.about")}</p>
-    </ div>
+        </ div>
+        </ div>
 }
 
 export default Main
